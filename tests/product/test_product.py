d9dc2b88867fca1b4c0e387ac52f0398de07386f
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-09 10:08:43 -0300

    test: req-01 - test_cria_produto implementado

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui - ciente
+    produto = Product(
+        1,
+        "CADEIRA",
+        "Forces of Nature",
+        "2022-04-04",
+        "2023-02-09",
+        "FR48",
+        "Conservar em local fresco",
+    )
+    assert produto.id == 1
+    assert produto.nome_da_empresa == "Forces of Nature"
+    assert produto.data_de_fabricacao == "2022-04-04"
+    assert produto.data_de_validade == "2023-02-09"
+    assert produto.numero_de_serie == "FR48"
+    assert produto.instrucoes_de_armazenamento == "Conservar em local fresco"
