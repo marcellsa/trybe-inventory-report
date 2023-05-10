20eb35001092885cacd6f24bf50b036f09ae0008
Marcel Albuquerque <msalbuquerque@hotmail.com>
2023-05-10 16:38:17 -0300

    feat: req-03 - método generate implementado na classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,13 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(data):
+        report = SimpleReport.generate(data)
+        report += "\nProdutos estocados por empresa:\n"
+        companies = Counter([item["nome_da_empresa"] for item in data])
+        for company, quantity in companies.items():
+            report += f"- {company}: {quantity}\n"
+        return report
